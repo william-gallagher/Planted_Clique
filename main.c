9d692844df5069a409064ed3aed38ad1a319963e
William Gallagher <william.online.seller@gmail.com>
2020-04-21 13:44:50 -0400

    First Commit

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
+#define n 4098
+
+struct{
+  int degree;
+  int neighbor[n];
+} typedef vertex;
 
 int main(){
 
+  
 
   printf("This is the master branch\n");
 
